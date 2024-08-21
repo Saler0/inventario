4fcd96d7687987c66843539dd69255aaa49dfdfa
Saler0 <milenko.espinozag@gmail.com>
2024-08-20 20:55:42 -0400

    Fix: añadiendo log al bat para conocer errores

--- Backend.bat
+++ Backend.bat
@@ -1,1 +1,7 @@
 start /B java -jar C:\ProgramData\Jenkins\.jenkins\workspace\inventarioPipeline\target\inventario-0.0.1-SNAPSHOT.jar
+timeout /T 5 /NOBREAK
+findstr /C:"Started" application.log
+if errorlevel 1 (
+    echo Application failed to start
+    exit /B 1
+)
